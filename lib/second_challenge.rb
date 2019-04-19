ee3cc313a74651f0acc49c66e778c7f5471686db
Mark May <mamark90@gmail.com>
2019-04-19 01:36:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,13 +5,12 @@ def second_challenge
                   :hero => {},
                   :hero_friends => []
 
-      #your key/value pairs here!
       },
     :capulet => { :patriarch =>{},
                   :matriarch =>{},
                   :heroine => {},
                   :heroine_friends => []
-      #your key/value pairs here!
+
       }}
 
 
